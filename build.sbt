a4f22bb6392a9a09ef4e25aedc34229e328c83c5
Shubham Bagrodia <shubham.bagrodia@nashtechglobal.com>
2023-09-17 00:33:31 +0530

    Created a dummy spark project

---
+++ build.sbt
@@ -0,0 +1,15 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.12.13"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "SparkApache"
+  )
+
+val sparkVersion = "3.3.2"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
+
---
+++ src/main/scala/com/Recap/Spark.scala
@@ -0,0 +1,19 @@
+package com.Recap
+
+import org.apache.spark.sql.SparkSession
+
+object Spark extends App {
+  val spark = SparkSession.builder()
+    .appName("DataFrames Basics")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val firstDF = spark.read
+    .format("json")
+    .option("inferSchema", "true")
+    .option("multiline", "true")
+    .load("src/main/resources/sampleData/population_data.json")
+
+  firstDF.show()
+  firstDF.printSchema()
+}
